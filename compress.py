da99d4139ee087f4e0d87832b847cfcf2e90665f
Charles Leclerc <leclerc.charles@gmail.com>
2017-11-12 16:22:59 -0500

    Ported compress.py to python 3 and switched to HTTPS connection

--- compress.py
+++ compress.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python
 
-import httplib, urllib, sys
+import http.client, urllib.request, urllib.parse, urllib.error, sys
 
 # Define the parameters for the POST request and encode them in
 # a URL-safe format.
 f = open('jquery.i18n.properties.js', 'r')
 
-params = urllib.urlencode([
-    #('js_code', sys.argv[1]),
+params = urllib.parse.urlencode([
     ('js_code', f.read()),
     ('compilation_level', 'SIMPLE_OPTIMIZATIONS'),
     ('output_format', 'text'),
     ('output_info', 'compiled_code'),
   ])
-
+f.close()
 # Always use the following value for the Content-type header.
 headers = { "Content-type": "application/x-www-form-urlencoded" }
-conn = httplib.HTTPConnection('closure-compiler.appspot.com')
+conn = http.client.HTTPSConnection('closure-compiler.appspot.com')
 conn.request('POST', '/compile', params, headers)
 response = conn.getresponse()
 data = response.read()
 
 #print data
-f2 = open('jquery.i18n.properties.min.js', 'w')
+f2 = open('jquery.i18n.properties.min.js', 'wb')
 f2.write(data)
+f2.close()
 
 conn.close
